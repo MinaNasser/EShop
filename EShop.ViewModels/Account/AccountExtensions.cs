30ddab9ea34c4a606283ab123ff1052c4b3b536e
MinaAl5al <minanasser82018@gmail.com>
2025-03-23 01:59:57 +0200

    Update Name Space

--- EShop.ViewModels/Account/AccountExtensions.cs
+++ EShop.ViewModels/Account/AccountExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EShop.ViewModels.Account
+namespace EShop.ViewModels
 {
     public static class AccountExtensions
     {
